6af4d99f3ecc3672dcaa5e16e28fd3434deb3d59
Chi Cong <81363889+kanie-reishi@users.noreply.github.com>
2023-07-05 12:04:55 +0700

    test

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,9 @@ fn main() -> Result<()> {
      if output.status.success() {
         let std_out = std::str::from_utf8(&output.stdout)?;
         let std_err = std::str::from_utf8(&output.stderr)?;
-         print!("{}", std_out.to_owned() + std_err);
+         print!("{}", std_out);
+         print!("{}", std_err);
      } else {
-        print!("Check");
          std::process::exit(1);
      }
 
